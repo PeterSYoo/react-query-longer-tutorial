b324ca60a19bf69062968d1b9aecc4d2515517fc
Peter Yoo <trustintofu@gmail.com>
2022-09-23 11:07:48 -0700

    wip: installs react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import { Link } from 'react-router-dom';
 import './App.css';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import HomePage from './components/home.page';
 import RQSuperHeroesPage from './components/rqsuperheroes.page';
 import SuperHeroesPage from './components/superheroes.page';
 import Nav from './components/nav.components';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/super-heroes" element={<SuperHeroesPage />} />
-          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/super-heroes" element={<SuperHeroesPage />} />
+            <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
   );
 }
