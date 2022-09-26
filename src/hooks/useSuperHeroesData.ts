b4cd0aea944a72335961679dfe316763c17807c3
Peter Yoo <trustintofu@gmail.com>
2022-09-25 19:11:54 -0700

    wip: adds query invalidation

--- src/hooks/useSuperHeroesData.ts
+++ src/hooks/useSuperHeroesData.ts
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from 'react-query';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
 import axios from 'axios';
 
 const fetchSuperHeroes = () => {
@@ -28,5 +28,10 @@ export const useSuperHeroesData = (onSuccess: any, onError: any) => {
 };
 
 export const useAddSuperHeroData = () => {
-  return useMutation(addSuperHero);
+  const queryClient = useQueryClient();
+  return useMutation(addSuperHero, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('super-heroes');
+    },
+  });
 };
